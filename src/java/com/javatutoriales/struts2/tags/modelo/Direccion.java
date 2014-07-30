9505f3e69aaa9fdaf0e25cfca36e583c1f0aea93
InmaGs <inmac.garciasanchez@gmail.com>
2014-07-30 14:05:53 +0200

    Preparación ejercicios de tags.

---
+++ src/java/com/javatutoriales/struts2/tags/modelo/Direccion.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Creamos clase que contiene los datos de la información que se va a mostrar
+
+package com.javatutoriales.struts2.tags.modelo;
+
+import java.io.Serializable;
+/**
+ *
+ * @author inmaculada.garcia
+ */
+public class Direccion implements Serializable{
+    
+    private String calle, codigoPostal;
+    
+    public String getCalle(){
+        return calle;
+    }
+    
+    public String getCodigoPostal(){
+        return codigoPostal;
+    }
+    
+    public void setCalle(String calle){
+        this.calle=calle;
+    }
+    
+    public void setCodigoPostal(String codigoPostal){
+        this.codigoPostal=codigoPostal;
+    }
+}
---
+++ src/java/com/javatutoriales/struts2/tags/modelo/Usuario.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Creación de la clase de la que vamos a obtener los datos necesarios
+package com.javatutoriales.struts2.tags.modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author inmaculada.garcia
+ */
+public class Usuario implements Serializable {
+    
+    private String nombre;
+    private int edad;
+    private Direccion direccion;
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    
+    public Direccion getDireccion(){
+        return direccion;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+    
+    public void setDireccion(Direccion direccion){
+        this.direccion=direccion;
+    }
+}
